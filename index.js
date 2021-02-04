02bc3a62b44ee98db7bef989655751afc7fdb5b8
Nate18244 <nath182182@hotmail.com>
2021-02-04 16:38:06 +0100

    what I have so far that works

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ Write a function "crazyDiff" that computes the absolute difference between a giv
 It should return triple their absolute difference if the given number is greater than 19.
 */
 
-/*function crazyDiff (num1, 19)  {
+function crazyDiff (num1, num2)  {
     let result;
-    if(num1 > 19) {
+    if(num1 > 19 && num2 > 19) {
         result = (num1 - num2) * 3
     } else {
         result = num1 - num2
@@ -57,13 +57,13 @@ It should return triple their absolute difference if the given number is greater
     
 }
 
-console.log("Crazy diff result: " + crazyDiff(5,2))
+console.log("Crazy diff result: " + crazyDiff(567,2))
 
 
 
 
 
-console.log("Final result is = " + crazyDiff(8,2)) */
+console.log("crazyDiff result 2 is = " + crazyDiff(8,2)) //Cant get it to do the greater than 19 sum :/
 
 
 
@@ -73,14 +73,16 @@ Write a function "boundary" which accept an integer n and returns true if n is w
 
 /* WRITE YOUR CODE HERE */
 
-/*function boundary (n) {
-    let result; 
-    if(n >= 20 && === 100) {
-        result = true;
+const boundary = function (n) {
+    
+    if(n >= 20 && n <=200) {
+        return true;
+    } else {
+        return ("I respectfully ask that you please enter a number between 20 and 200. Thank you")
     }
-}*/
+}
 
-//console.log(boundary(45))
+console.log(boundary(687))
 
 /*EXERCISE 5
 Write a function "strivify" which accepts a string.
@@ -96,6 +98,15 @@ HINT: Module Operator
 
 /* WRITE YOUR CODE HERE */
 
+const checkNumber = function (x) { // x has been defined here surely 
+    if (x % 3 && x % 7) {
+        return "this is a multiple of 3 & 7";
+    }
+}
+
+
+console.log(checkNumber(3)) // I don´t understand why this is undefined
+
 /* EXERCISE 7
 Write a function "reverseString" to programmatically reverse a given string (es.: Strive => evirtS).*/
 
@@ -109,3 +120,50 @@ const reverseString = (str) => {
 }
 
 console.log(reverseString("This was very annoying"));
+
+/* EXERCISE 8
+Write a function "upperFirst" to capitalize the first letter of each word of a given string passed as a parameter.
+*/
+
+const toUpperCase = function (str) {
+    str = str.substring(1) 
+    return str
+}
+
+console.log(toUpperCase("i thought substring would work, evidently not"))
+
+
+
+
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 9
+Write a function "cutString" to create a new string without the first and last character of a given string.
+*/
+
+
+
+/* WRITE YOUR CODE HERE */
+
+const cutString = function (str) {
+    str = str.slice(1, -1) // slice is a method, start at one and then cut at last character which will be -1.
+    return str
+}
+
+console.log(cutString("Nathan")) // Prints "atha"
+
+/* EXERCISE 10
+Write a function "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10.
+*/
+
+const giveMeRandom = function(n) { //accepts any number
+    let returnedNumber = [];          //empty array to push the result
+    for(i = 0; i < n; i++) {       // "regular" for loop
+        returnedNumber.push(Math.floor(Math.random() * 10)); // push into variable,, math floor gives a whole number and math random is what it says it is 
+    }
+
+    return returnedNumber
+    
+}
+console.log(giveMeRandom(9)); //*borat voice* GREAT SUCCESS
